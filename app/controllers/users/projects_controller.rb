e3b04c444ba71ceb7ae15bb11aaa2831a51ff27a
memoxmrdl <jmemox@gmail.com>
2017-10-09 15:33:04 -0500

    Refactor

--- app/controllers/users/projects_controller.rb
+++ app/controllers/users/projects_controller.rb
@@ -1,10 +1,11 @@
 class Users::ProjectsController < Users::BaseController
+  before_action :find_project, except: [:index, :new]
+
   def index
     @projects = current_user.projects
   end
 
   def show
-    @project = current_user.projects.find(params[:id])
   end
 
   def new
@@ -22,12 +23,9 @@ class Users::ProjectsController < Users::BaseController
   end
 
   def edit
-    @project = current_user.projects.find(params[:id])
   end
 
   def update
-    @project = current_user.projects.find(params[:id])
-
     if @project.update(project_params)
       redirect_to users_projects_path, notice: t('.notice')
     else
@@ -36,8 +34,6 @@ class Users::ProjectsController < Users::BaseController
   end
 
   def destroy
-    @project = current_user.projects.find(params[:id])
-
     @project.destroy
 
     redirect_to users_projects_path, notice: t('.notice')
@@ -47,5 +43,9 @@ class Users::ProjectsController < Users::BaseController
 
   def project_params
     params.require(:project).permit(:title, :short_description, :description, :image)
+  end
+
+  def find_project
+    @project = current_user.projects.find(params[:id])
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  DEFAULT_STYLES = { thumb: '64x64#', small: '470x470#' }
+  IMAGE_DEFAULT_STYLES = { thumb: '64x64#', small: '470x470#' }
 
   belongs_to :user
 
@@ -8,7 +8,7 @@ class Project < ActiveRecord::Base
             :short_description, presence: true
 
   has_attached_file :image,
-                    styles: DEFAULT_STYLES,
+                    styles: IMAGE_DEFAULT_STYLES,
                     default_url: lambda { |file| file.instance.paperclip_set_default_url }
   validates_attachment_presence :image
   validates_attachment_size :image, less_than: 0..2024.kilobytes
