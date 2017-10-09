6cca52d5909af754196606388bd0cfce3248cb9a
memoxmrdl <jmemox@gmail.com>
2017-10-09 14:10:07 -0500

    Created Users::ProjectsController#show

--- app/controllers/users/projects_controller.rb
+++ app/controllers/users/projects_controller.rb
@@ -2,4 +2,8 @@ class Users::ProjectsController < Users::BaseController
   def index
     @projects = current_user.projects
   end
+
+  def show
+    @project = current_user.projects.find(params[:id])
+  end
 end
